73d80b37e6ae63862a1dde55ddbd1c7b969762f8
Oyinbo David <dev.davexoyinbo@gmail.com>
2021-09-16 19:39:09 +0100

    .

--- layouts/authenticated.vue
+++ layouts/authenticated.vue
@@ -1,10 +1,19 @@
 <template>
   <div id="authenticated-layout">
-    <div id="side-nav-container">
-      <div class="backdrop"></div>
+    <div
+      id="side-nav-container"
+      ref="navContainer"
+      :class="{ active: sideBarOn }"
+    >
+    <span class="menu-button close" @click="sideBarOn = false"
+        ><i class="fas fa-times"></i> </span>
+      <div class="backdrop" @click="sideBarOn = false"></div>
       <side-nav />
     </div>
     <div id="layout-content">
+      <span class="menu-button" @click="sideBarOn = true"
+        ><i class="fas fa-bars"></i
+      ></span>
       <Nuxt />
     </div>
   </div>
@@ -17,22 +26,32 @@ import SideNav from "~/components/authenticated/SideNav.vue";
 @Component({
   components: { SideNav },
 })
-export default class AuthenticatedLayout extends Vue {}
+export default class AuthenticatedLayout extends Vue {
+  sideBarOn: boolean = false;
+}
 </script>
 
 <style lang="scss">
+@import "~assets/styles/variables";
 @import "~assets/styles/shadows";
 
 #authenticated-layout {
   padding: 12px 8px;
   @media only screen and (min-width: 768px) {
     padding: 12px 24px;
-
   }
   display: flex;
   gap: 1.5rem;
   min-height: 100vh;
   max-height: 100vh;
+  .menu-button {
+    padding: 1rem;
+    display: inline-block;
+    cursor: pointer;
+    @media only screen and (min-width: 768px) {
+      display: none;
+    }
+  }
   #layout-content {
     flex: 1;
     background: white;
@@ -46,8 +65,25 @@ export default class AuthenticatedLayout extends Vue {}
     top: 12px;
     bottom: 12px;
     // right: 100%;
-    left: 0;
+    left: -250px;
     z-index: 10;
+    width: 250px;
+    transition: all 0.2s ease-in-out;
+
+    .menu-button.close {
+      position: absolute;
+      top: 0;
+      left: calc(100% + 5px);
+      background: $secondary-color-2;
+      border-radius: 50%;
+      padding: 0;
+      height: 40px;
+      width: 40px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      display: none;
+    }
     .backdrop {
       position: fixed;
       top: 0;
@@ -56,15 +92,41 @@ export default class AuthenticatedLayout extends Vue {}
       left: 0;
       background: rgb(0 0 0 / 70%);
       z-index: -1;
+      opacity: 0;
+      pointer-events: none;
+      transition: all 0.2s ease-in-out;
     }
-    display: none;
     @media only screen and (min-width: 768px) {
+      width: unset;
       position: initial;
       display: block;
 
       .backdrop {
         display: none;
       }
+    }
+
+    &.active {
+      left: 12px;
+      .backdrop {
+        opacity: 1;
+        pointer-events: initial;
+      }
+
+      .menu-button.close {
+      position: absolute;
+      top: 0;
+      left: calc(100% + 5px);
+      background: $secondary-color-2;
+      border-radius: 50%;
+      padding: 0;
+      height: 40px;
+      width: 40px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      display: inline-flex;
+    }
     }
   }
 }
